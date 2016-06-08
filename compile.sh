6bae95c9bed65cba5ffa03a766c7988fb904f92f
jbkr <baker.james.jb@gmail.com>
2016-06-08 11:18:38 +0800

    OS specific compiler routines

--- compile.sh
+++ compile.sh
@@ -1,4 +1,23 @@
-pdflatex thesis.tex
-bibtex thesis.aux
-pdflatex thesis.tex
-pdflatex thesis.tex
+platform='unknown'
+unamestr=`uname`
+platform='unknown'
+unamestr=`uname`
+if [[ "$unamestr" == 'Linux' ]]; then
+   platform='linux'
+elif [[ "$unamestr" == 'Darwin' ]]; then
+   platform='darwin'
+fi
+
+if [[ $platform == 'linux' ]]; then
+  pdflatex thesis.tex
+  bibtex thesis.aux
+  pdflatex thesis.tex
+  pdflatex thesis.tex
+
+
+elif [[ $platform == 'darwin' ]]; then
+  xelatex thesis.tex
+  bibtex thesis.aux
+  xelatex thesis.tex
+  xelatex thesis.tex
+fi
